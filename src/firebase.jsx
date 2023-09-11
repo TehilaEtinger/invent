85e6d76278e472f61c6c81d9f964eeb147bb66ec
TehilaEtinger <tehilla4468@gmail.com>
2023-09-12 01:04:58 +0300

    .env

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -9,13 +9,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyA_Fw9ggsDW7orLQuBWoXTPFGDt-HRjBsI",
-  authDomain: "onegshabat-8ee62.firebaseapp.com",
-  projectId: "onegshabat-8ee62",
-  storageBucket: "onegshabat-8ee62.appspot.com",
-  messagingSenderId: "83605291190",
-  appId: "1:83605291190:web:1630362da566f15a20a617",
-  measurementId: "G-WJ4P43G7EF"
+  apiKey: import.meta.env.VITE_REACT_APP_API_KEY,
+  authDomain: import.meta.env.VITE_REACT_APP_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_REACT_APP_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_REACT_APP_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_REACT_APP_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_REACT_APP_APPID,
+  measurementId: import.meta.env.VITE_REACT_APP_MEASUREMENTID
 };
 
 // Initialize Firebase
